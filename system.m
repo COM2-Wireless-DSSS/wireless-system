67530b13f0c45d4739b75f564a42fa6b90de1582
camillabarreto <camillabarretodesousa@gmail.com>
2021-07-23 09:38:55 -0300

    iniciando um rascunho

---
+++ system.m
@@ -0,0 +1,17 @@
+close all; clear all; clc;
+
+## rascunho TX
+
+# 1 gerar sinal de transmissao
+# 2 gerar codigo de sequencia (7 chips)
+# 3 modular ( M-PSK, M={4,8,16} )
+# 4 espalhar
+# 5 gerar 2 canais (Rician)
+# 6 transmitir
+
+
+## rascunho RX
+
+# 1 desespalhar
+# 2 combinar componentes (MRC)
+# 3 demodular
