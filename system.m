c4da3e4e3ca19b9b8b526670399a004a55b92f6b
Camilla Barreto de Sousa <42206341+camillabarreto@users.noreply.github.com>
2021-07-29 00:43:59 -0300

    processo de espalhar e desespalhar (spread, reconstruct)

--- system.m
+++ system.m
@@ -1,17 +1,37 @@
 close all; clear all; clc;
 
-## rascunho TX
+%%% rascunho TX
 
-# 1 gerar sinal de transmissao
-# 2 gerar codigo de sequencia (7 chips)
-# 3 modular ( M-PSK, M={4,8,16} )
-# 4 espalhar
-# 5 gerar 2 canais (Rician)
-# 6 transmitir
+%%------------------------------------------------| MODULAR ( M-PSK, M={4,8,16} )
+% M = [4 8 16]; m = pskmod(x,M(3)); scatterplot(m);
+
+%%------------------------------------------------| ESPALHAR
+ff= 2; % qtd de flip-flops (ff=3 => 7 chips)
+C = 2^ff-1; % qtd de chips de espalhamento
+MSCode = mls(C,0)  % codigo de sequencia maxima (0b -> 1; 1b -> -1) (flag=0 default)
+
+nBits = 10;
+info_bin = randi([0 1], 1, nBits)
+info = (info_bin'*2)-1 % bits de informacao (0b -> 1; 1b -> -1) 
+
+spread = (info*MSCode)' % espalhando o sinal (0b -> 1; 1b -> -1)
+spread_bin = (reshape(-spread,1,[])+1)/2 % transformando pra binario
+
+%%------------------------------------------------| GERAR 2 CANAIS (Rician)
+
+%%------------------------------------------------| TRANSMITIR
 
 
-## rascunho RX
 
-# 1 desespalhar
-# 2 combinar componentes (MRC)
-# 3 demodular
+%%% rascunho RX
+
+%%------------------------------------------------| DESESPALHAR
+
+r = (reshape(spread_bin, 7, nBits)*2)-1 % 7 para ff=2
+reconstruct = MSCode*r
+reconstruct_bin = reconstruct<0
+info_bin % comparando
+
+%%------------------------------------------------| COMBINAR COMPONENTES (MRC)
+%%------------------------------------------------| DEMODULAR
+% d = pskdemod(m,M(3));
